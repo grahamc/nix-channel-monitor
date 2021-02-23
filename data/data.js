02cb232312c8044d0ced4b913f05c1fb09bfee47
Buildkite <ci@buildkite>
2021-02-23 13:57:25 +0000

    Automatic channel data update.

--- data/data.js
+++ data/data.js
@@ -721,6 +721,10 @@ const graphData = [
          [
             "7b9655158eb91c8fdd0d9f97a6a8ee812f2b1ead",
             "2021-02-23T06:21:13Z"
+         ],
+         [
+            "1ec1a234d893b110b09939390b00d9b6e0b95a4d",
+            "2021-02-23T13:57:13Z"
          ]
       ],
       "name" : "nixos-unstable-small"
