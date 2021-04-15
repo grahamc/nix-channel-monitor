6ee4dea18505ab247dc77516941a62d7cd8bd755
Buildkite <ci@buildkite>
2021-04-15 18:01:31 +0000

    Automatic channel data update.

--- data/data.js
+++ data/data.js
@@ -2387,10 +2387,6 @@ const graphData = [
    {
       "history" : [
          [
-            "97a13fb97fcda79f9ab3ee73b5dca65eb891c11a",
-            "2021-01-15T18:00:42Z"
-         ],
-         [
             "1eff582e7ea6dcb208127ad369e231eaaf6fa378",
             "2021-01-16T12:25:20Z"
          ],
