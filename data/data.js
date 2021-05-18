f393460a565cb744bcec29dfbe840cd576f7a4ca
Buildkite <ci@buildkite>
2021-05-18 02:41:43 +0000

    Automatic channel data update.

--- data/data.js
+++ data/data.js
@@ -2953,6 +2953,10 @@ const graphData = [
          [
             "accb1763abada1afb6517b67206460de8a70bd3e",
             "2021-05-17T14:31:41Z"
+         ],
+         [
+            "42fcae2f87becb203bcdad5d4cb8fb261216c55c",
+            "2021-05-18T02:41:32Z"
          ]
       ],
       "name" : "nixpkgs-20.09-darwin"
