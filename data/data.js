4c0e4b36b3107518931545413aa97b7fd5a0e339
Buildkite <ci@buildkite>
2021-06-28 03:11:33 +0000

    Automatic channel data update.

--- data/data.js
+++ data/data.js
@@ -2258,6 +2258,10 @@ const graphData = [
          [
             "1fbbf2eda316c398c69cdabf2eeeba9bf72dcea5",
             "2021-06-27T11:21:35Z"
+         ],
+         [
+            "59b8d9cf24e9fcf10341a0923c9bdca088dca8c8",
+            "2021-06-28T03:11:17Z"
          ]
       ],
       "name" : "nixos-20.09"
