2038468bd93e026945900c3d66696051cc2a3999
Buildkite <ci@buildkite>
2021-07-18 18:21:27 +0000

    Automatic channel data update.

--- data/data.js
+++ data/data.js
@@ -1290,6 +1290,10 @@ const graphData = [
          [
             "2489eb5e4516aab575ab114b7e0a3e1b5c5daca7",
             "2021-07-18T01:01:22Z"
+         ],
+         [
+            "64c4e4bccebc056e0156ad7b822cdec87e29a87a",
+            "2021-07-18T18:21:15Z"
          ]
       ],
       "name" : "nixpkgs-unstable"
