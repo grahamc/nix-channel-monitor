8524477d0ead4180fd249dfd65b1aa4b9e778ab0
Buildkite <ci@buildkite>
2022-08-27 07:02:23 +0000

    Automatic channel data update.

--- data/data.js
+++ data/data.js
@@ -805,6 +805,10 @@ const graphData = [
          [
             "d50c2c2f895d7da93bee5df2382d8f569889c47d",
             "2022-08-27T00:51:57Z"
+         ],
+         [
+            "44e3d2f871c4de7a3d4ed5c28fa18af8444a992b",
+            "2022-08-27T07:02:06Z"
          ]
       ],
       "name" : "nixos-unstable-small"
@@ -1342,19 +1346,19 @@ const graphData = [
       "history" : [
          [
             "0d68d7c857fe301d49cdcd56130e0beea4ecd5aa",
-            "2022-06-21T13:21:55Z"
+            "2022-06-21T13:21:56Z"
          ]
       ],
-      "name" : "nixos-test-staging"
+      "name" : "nixos-test-staging-baseline"
    },
    {
       "history" : [
          [
             "0d68d7c857fe301d49cdcd56130e0beea4ecd5aa",
-            "2022-06-21T13:21:56Z"
+            "2022-06-21T13:21:55Z"
          ]
       ],
-      "name" : "nixos-test-staging-baseline"
+      "name" : "nixos-test-staging"
    },
    {
       "history" : [
